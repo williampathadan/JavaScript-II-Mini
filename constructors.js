d2c02c3f1cebf193598b08bd8689fa48dd768d2f
William Antony <32254679+williampathadan@users.noreply.github.com>
2017-11-08 16:46:39 -0600

    Completed constructor.js

--- constructors.js
+++ constructors.js
@@ -20,16 +20,19 @@ Animal.prototype.grow = function () {
 
 function Cat(options) {
   // invoke Animal here with .call
+  Animal.call(this, options);
 }
 
 // connect the prototypes here
+Cat.prototype = Object.create(Animal.prototype);
+
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
+const foofie = new Cat({
+  name: 'foofie',
+});
 //
-// foofie.grow();
+foofie.grow();
 
